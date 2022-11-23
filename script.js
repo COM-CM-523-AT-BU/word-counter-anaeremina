dd30b85ae4aa7a3b123af5e6df1142e4bb8f3d62
anaeremina <eremina@bu.edu>
2022-11-22 23:30:31 -0500

    changes

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 // put your globals here - hint: select elements from the HTML
 
+const textArray = document.getElementById("form-label");
 
-// dont' forget to add event listeners to teh buttons
+// don't forget to add event listeners to teh buttons
+
+checkLength.addEventListener("click", countWords);
 
 function countWords() {
-  // your code here
-  // call printData
+// your code here
+    // call printData
+    const words = textblock.split(' ');
+    let count = 0;
+     for (var i = 0; i < words; i++) {
+      if (word[i]==" "){
+        count ++;
+      }
+    }
+    printData(count);
 }
 
+  
 function findWords() {
   // put your local variable for the empty array here
   // remainder of your code follows
   // call printData
+  const words = textblock.split(' ');
+  let wordsRepeated = 0;
+  while (word != -1) {
+    word = graf.indexOf("e", i + 1);
+    num += 1;
+    i = wordsRepeated;
+}
+  printData(wordsRepeated)
 }
 
 // change param1 and param2 to identifiers that make sense
-function printData(param1, param2) {
+function printData(countWords, findWords) {
   // your code here... one line!
 }
