ff91efd95e4619c18a11edd3d384ffb87b36dd5c
anaeremina <eremina@bu.edu>
2022-11-24 16:50:15 -0500

    Changes:Ana

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
 // put your globals here - hint: select elements from the HTML
 
 const text = document.getElementById("text-area");
-const textOutput = document.getElementById("wordCountInput");
-const find = document.getElementById("findWordInput");
-const findOutput = document.getElementById("wordFoundInput");
-
+const wordCount = document.getElementById ("wordCountInput");
+const countBtn = document.getElementById ("countBtn");
+const wordFind = document.getElementById ("wordFoundInput");
+const findBtn = document.getElementById ("findWords");
 
 // don't forget to add event listeners to the buttons
 
-text.addEventListener("click", countWords);
-find.addEventListener("click", findWords);
+countBtn.addEventListener ("click", countWords);
+findBtn.addEventListener ("click", countWords);
 
 // function that counts words
-
 function countWords() {
-  // your code here
-      let conversion = text.toString();
-      let words = conversion.split(' ');
-      let count = 0;  
-       for (var i = 0; i < words.length; i++) {
-        if (words[i] != ""){
-          count +=1; 
+  let conversion = text.toString();
+  let words = conversion.split(' ');
+        let count = 0;  
+         for (var i = 0; i < words.length; i++) {
+          if (words[i] != ""){
+            count +=1; 
+          }
         }
-      }
-      // call printData
-      printData(count, findOutput);
+        // call printData
+        printData(count, wordCount);
+    }
+  
+     // count instances of a word, use filter or forEach
+  function findWords() {
+    let foundWords = [];
+    // put your local variable for the empty array here
+    // remainder of your code follows
+    for(var i = 0; i < text.length; ++i) {
+      if(!foundWords[text[i]])
+      foundWords[text[i]] = 0;   
+      ++foundWords[text[i]];
   }
-
-   // count instances of a word, use filter or forEach
-function findWords() {
-  let foundWords = [];
-  // put your local variable for the empty array here
-  // remainder of your code follows
-  for(var i = 0; i < text.length; ++i) {
-    if(!foundWords[text[i]])
-    foundWords[text[i]] = 0;   
-    ++foundWords[text[i]];
-}
- // call printData
-  printData(foundWords, textOutput);
-}
-
-// change param1 and param2 to identifiers that make sense
-function printData(message, destination) { 
-  destination.value = message; 
-}
-
+   // call printData
+    printData(foundWords, wordFind);
+  }
+  
+  // change param1 and param2 to identifiers that make sense
+  function printData(message, destination) { 
+    destination.value = message; 
+  }
+  
